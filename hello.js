e90d7739e64b39dfdb887f70731d3532e89075f3
Buxin-Emilo <zgsxiaohao@gmail.com>
2025-12-05 21:45:01 +0100

    first commit

---
+++ hello.js
@@ -0,0 +1,23 @@
+function hello(name) {
+  let phrase = `hello,${name}`;
+  debugger;
+  say(phrase);
+}
+function say(phrase) {
+  alert(`** ${phrase}`);
+}
+
+let str = `
+  ECMA International's TC39 is a group of JavaScript developers,
+  implementers, academics, and more, collaborating with the community
+  to maintain and evolve the definition of JavaScript.
+`;
+alert(str);
+
+show(
+  parameters,
+  aligned, // 左边有 5 个空格
+  one,
+  after,
+  another
+);
