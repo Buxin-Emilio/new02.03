a02523cc2991b0b252608c78a48c6e23a8cdd345
Buxin-Emilo <zgsxiaohao@gmail.com>
2025-12-06 20:51:34 +0100

    0.01

--- hello.js
+++ hello.js
@@ -1,23 +1,33 @@
-function hello(name) {
-  let phrase = `hello,${name}`;
-  debugger;
-  say(phrase);
-}
-function say(phrase) {
-  alert(`** ${phrase}`);
-}
+// function hello(name) {
+//   let phrase = `hello,${name}`;
+//   debugger;
+//   say(phrase);
+// }
+// function say(phrase) {
+//   alert(`** ${phrase}`);
+// }
 
-let str = `
-  ECMA International's TC39 is a group of JavaScript developers,
-  implementers, academics, and more, collaborating with the community
-  to maintain and evolve the definition of JavaScript.
-`;
-alert(str);
+// let str = `
+//   ECMA International's TC39 is a group of JavaScript developers,
+//   implementers, academics, and more, collaborating with the community
+//   to maintain and evolve the definition of JavaScript.
+// `;
+// alert(str);
 
-show(
-  parameters,
-  aligned, // 左边有 5 个空格
-  one,
-  after,
-  another
-);
+// show(
+//   parameters,
+//   aligned, // 左边有 5 个空格
+//   one,
+//   after,
+//   another
+// );
+function showPrimes(n) {
+  nextPrime: for (let i = 2; i < n; i++) {
+    // 检测 i 是否是一个质数（素数）
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+
+    alert(i);
+  }
+}
